31316b436cf9e2019a27612864490145a3c77c31
JJJong <alswhdwjd1@naver.com>
2020-11-06 09:29:29 +0900

    intent에 null 값 넣음

--- app/src/main/java/com/example/design01/ThirdScreen01.kt
+++ app/src/main/java/com/example/design01/ThirdScreen01.kt
@@ -8,6 +8,6 @@ class ThirdScreen01 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_screen01)
-        val Intent : Intent =
+        val Intent : Intent? = null
     }
 }
